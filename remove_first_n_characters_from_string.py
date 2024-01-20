47f948dc04ebede9962f5c8a72eec87e54c7d36c
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-20 22:23:44 +0800

    Ask user for a word and number

--- remove_first_n_characters_from_string.py
+++ remove_first_n_characters_from_string.py
@@ -2,12 +2,18 @@
 
 # pseudocode
 
-# write a message about what the program does
-
 # get user input for a string
+word = input("Enter a word: ")
 
 # get user input for the number of characters to remove
+num = int(input("Enter the number of characters to remove: "))
+
+print(word)
+print(num)
+# write a message about what the program does
+
 
 # remove the number of characters from the string
+
 
 # print the new string
