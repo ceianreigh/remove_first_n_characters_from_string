8165ebcc7684cb5bf9ef147a3260566c25bfa4ba
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-20 22:25:03 +0800

    Remove first n characters from a string

--- remove_first_n_characters_from_string.py
+++ remove_first_n_characters_from_string.py
@@ -8,12 +8,11 @@ word = input("Enter a word: ")
 # get user input for the number of characters to remove
 num = int(input("Enter the number of characters to remove: "))
 
-print(word)
-print(num)
 # write a message about what the program does
-
+print("Removing the first", num, "characters from the string.")
 
 # remove the number of characters from the string
-
+new_word = word[num:]
 
 # print the new string
+print(new_word)
