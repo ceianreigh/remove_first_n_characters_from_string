ac68ac0773be4cc747b0baf9ee88c66cd7bfe933
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-20 22:16:50 +0800

    Add the instruction and pseudocode

---
+++ remove_first_n_characters_from_string.py
@@ -0,0 +1,13 @@
+# Write a program to remove characters from a string starting from zero up to n and return a new string.
+
+# pseudocode
+
+# write a message about what the program does
+
+# get user input for a string
+
+# get user input for the number of characters to remove
+
+# remove the number of characters from the string
+
+# print the new string
